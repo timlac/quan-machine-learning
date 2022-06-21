8bea69825f678ae0b431251aea8bd7d9fea078fd
timlac <tim@lachmann.nu>
2022-06-21 19:26:57 +0200

    added some functions for error and ver

--- python/csv_concat.py
+++ python/csv_concat.py
@@ -4,11 +4,20 @@ from pathlib import Path
 import re
 import pandas as pd
 
+
+class ErrorFileException(Exception):
+    def __init__(self, filename, message="Filename {} contains error marker, skipping file"):
+        self.filename = filename
+        self.message = message
+        super().__init__(self.message.format(self.filename))
+
+
 # filenames with mixed emotions contains the word mix
 # filenames with neutral emotion contains the word neu
 special_cases = {
     "mixed_emotions": "mix",
     "neutral_emotion": "neu",
+    "error": "e"
 }
 
 
@@ -20,9 +29,13 @@ def create_dataframe(paths):
     df = pd.DataFrame()  # Initialize empty data frame
 
     for filename in tqdm(paths):
-        df_tmp = pd.read_csv(filename)
-        df_tmp = set_df_columns(df_tmp, filename)
-        df = pd.concat([df, df_tmp])
+        try:
+            df_tmp = pd.read_csv(filename)
+            df_tmp = set_df_columns(df_tmp, filename)
+            df = pd.concat([df, df_tmp])
+        except ErrorFileException as e:
+            print(e)
+            continue
     return df
 
 
@@ -38,13 +51,22 @@ def set_df_columns(df, filename):
 
     df["video_id"] = name_list[0]
     df["file"] = Path(filename).name
+    # some videos are recorded in multiple versions, set 1 as default for all videos
+    df['version'] = 1
+    # neutral emotions are recorded in multiple situations, set 1 as default for all videos
+    df['situation'] = 1
 
     if name_list[1] == special_cases["mixed_emotions"]:
         df = set_mixed_emotions(df, name_list)
     elif name_list[1] == special_cases["neutral_emotion"]:
         df = set_neutral_emotion(df, name_list)
     elif len(name_list) > 4:
-        df = set_long_name(df, name_list)
+        if name_list[4] == special_cases["error"]:
+            raise ErrorFileException(filename)
+        elif name_list[4].startswith("ver"):
+            df = set_versioned_emotion(df, name_list)
+        else:
+            df = set_long_name(df, name_list)
     else:
         df = set_default_emotion(df, name_list)
     return df
@@ -62,6 +84,14 @@ def remove_extension(file):
 
 def name2list(file_name):
     return file_name.split("_")
+
+
+def get_digits_only(mixed_string):
+    """
+    :param mixed_string: some string that may contain digits and characters
+    :return: only digits
+    """
+    return re.sub("\\D", "", mixed_string)
 
 
 def set_mixed_emotions(df, name_list):
@@ -100,13 +130,22 @@ def set_default_emotion(df, name_list):
     return df
 
 
+def set_versioned_emotion(df, name_list):
+    df["mix"] = False
+    df['emotion_1'] = name_list[1]
+    df['vocalization'] = name_list[2]
+    df['intensity_level'] = name_list[3]
+    df['version'] = get_digits_only(name_list[4])
+    return df
+
+
 def main():
     input_path = "../files/tests/csv_concat/"
     paths = get_csv_paths(input_path)
     print(paths)
     df = create_dataframe(paths)
     print("saving csv")
-    df.to_csv("../files/tests/csv_concat/csv_concat_test.csv")
+    df.to_csv("../files/tests/out/csv_concat_test.csv")
 
 
 main()
--- python/run_opensmile.py
+++ python/run_opensmile.py
@@ -1,8 +1,2 @@
-#!/usr/bin/env python
-import sys
+import glob
 
-if __name__ == "__main__":
-    print("Hello, World!")
-    print(sys.argv[0])
-    print(sys.argv[1])
-    print(sys.argv[2])
