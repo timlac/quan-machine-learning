af8a1878422f470a236556c05b2d3ed9fa9beb24
timlac <tim@lachmann.nu>
2022-06-21 13:49:44 +0200

    testing

--- file_loop.sh
+++ file_loop.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # NOTE : Quote it else use array to avoid problems #
-export DATA_MOUNT=/home/tim/Videos
+export DATA_MOUNT=/media/tim/Seagate\ Backup\ Plus\ Drive/
 
-docker-compose -f /home/tim/work/su-thesis-project/OpenFace/docker-compose.yml up -d openface && sync # sync is to wait till service starts
+OUT_PATH=$DATA_MOUNT/Test/out
+DOCKER_COMPOSE_PATH=/home/tim/work/su-thesis-project/OpenFace/docker-compose.yml
 
-FILES="$DATA_MOUNT/A220_normalized_audiovideo_clips/*"
+docker-compose -f "$DOCKER_COMPOSE_PATH" up -d openface && sync # sync is to wait till service starts
+
+FILES="$DATA_MOUNT/Test/*"
 for f in $FILES
 do
-  docker exec -it openface FeatureExtraction -2Dfp -3Dfp -pdmparams -pose -aus -gaze -f $f -out_dir $DATA_MOUNT/out
-  docker exec -it openface chown -R $UID:$UID $DATA_MOUNT # chown to current user
+  docker exec -it openface FeatureExtraction -2Dfp -3Dfp -pdmparams -pose -aus -gaze -f "$f" -out_dir "$OUT_PATH"
+  docker exec -it openface chown -R $UID:$UID "$DATA_MOUNT" # chown to current user
 done
 
-docker-compose -f /home/tim/work/su-thesis-project/OpenFace/docker-compose.yml down
+docker-compose -f $DOCKER_COMPOSE_PATH down
