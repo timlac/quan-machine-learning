e4c57497ea01dc06a885372901afe4118419c775
timlac <tim@lachmann.nu>
2022-06-22 23:06:34 +0200

    added function to skip already processed files

--- file_loop.sh
+++ file_loop.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 # NOTE : Quote it else use array to avoid problems #
-export DATA_MOUNT=/media/tim/Seagate\ Backup\ Plus\ Drive/Test
-#export DATA_MOUNT=/home/tim/Videos
+export DATA_MOUNT=/media/tim/Seagate\ Backup\ Plus\ Drive
 
-OUT_PATH=$DATA_MOUNT/out_test
+OUT_PATH=$DATA_MOUNT/Out
 DOCKER_COMPOSE_PATH=/home/tim/work/su-thesis-project/OpenFace/docker-compose.yml
 
 docker-compose -f "$DOCKER_COMPOSE_PATH" up -d openface && sync # sync is to wait till service starts
 
-find "$DATA_MOUNT" -name "*.mov" -print0 | while IFS= read -r -d '' file; do
-    echo "file = $file"
+# create array of all elements in OUT_PATH to skip these later
+mapfile -d $'\0' array < <(find "$OUT_PATH" -type f -iname "*.csv" -exec basename {} .csv ";")
+
+find "$DATA_MOUNT/Documents" -name "*.mov" -print0 | while IFS= read -r -d '' file; do
+
+    echo processing file: "$file"
+
+    # get filename without extension
+    filename=$(basename -- "$file")
+    filename_no_extension="${filename%.*}"
+
+    # if filename_no_extension in OUT_PATH array, then continue, e.g. skip it
+    if [[ " ${array[*]} " =~ (^|[[:space:]])"$filename_no_extension"($|[[:space:]])  ]]; then
+        continue
+    fi
+
     docker exec openface FeatureExtraction -2Dfp -3Dfp -pdmparams -pose -aus -gaze -f "$file" -out_dir "$OUT_PATH"
     docker exec openface chown -R $UID:$UID "$DATA_MOUNT" # chown to current user
 done
 
 docker-compose -f $DOCKER_COMPOSE_PATH down
-
-#
-#for f in $FILES
-#do
-#  echo "$f"
-#
-##  docker exec -it openface FeatureExtraction -2Dfp -3Dfp -pdmparams -pose -aus -gaze -f "$f" -out_dir "$OUT_PATH"
-##  docker exec -it openface chown -R $UID:$UID "$DATA_MOUNT" # chown to current user
-#done
