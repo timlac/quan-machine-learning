716298270eb9acfe4634972d4a726715654746f2
timlac <tim@lachmann.nu>
2022-11-17 10:51:04 +0100

    rewrote group creation

--- src/preprocessing/dataset_creation/group_creation.py
+++ src/preprocessing/dataset_creation/group_creation.py
@@ -1,6 +1,64 @@
+import os
 import random
+
+import numpy as np
+import pandas as pd
+
 from global_config import seed, ROOT_DIR
 from sklearn.utils import shuffle
+
+from src.preprocessing.dataset_creation.helpers import slice_by, get_fixed_col
+
+
+def group_creation_desc(video_ids, groups, group_mapper):
+    video_ids_unique, video_ids_counts = np.unique(video_ids, return_counts=True)
+    counts_per_video_id = dict(zip(video_ids_unique, video_ids_counts))
+
+    groups_unique, groups_counts = np.unique(groups, return_counts=True)
+    counts_per_group = dict(zip(groups_unique, groups_counts))
+
+    print("Group assignment:")
+    print(group_mapper)
+
+    print("Number of videos per video id:")
+    print(counts_per_video_id)
+
+    print("Number of videos per group")
+    print(counts_per_group)
+
+
+def create_groups(video_ids, n_groups):
+    group_mapper = {}
+
+    for idx, video_id in enumerate(np.unique(video_ids)):
+        group_mapper[video_id] = idx % n_groups
+
+    groups = []
+    for video_id in video_ids:
+        group = group_mapper[video_id]
+        groups.append(group)
+
+    groups = np.array(groups)
+
+    group_creation_desc(video_ids, groups, group_mapper)
+
+    if not groups.shape == video_ids.shape:
+        raise ValueError("something went wrong, group vector size is not the same as video id vector")
+    else:
+        return groups
+
+
+def main():
+    df = pd.read_csv(os.path.join(ROOT_DIR, "files/out/query_13_videos.csv"))
+    slices = slice_by(df, "filename")
+    video_ids = get_fixed_col(slices, "video_id")
+    groups = create_groups(video_ids, 5)
+
+
+if __name__ == "__main__":
+    main()
+
+# TODO: Get rid of all of this and just create 5 groups with equal amount of video ids in them
 
 
 def get_evens(size):
