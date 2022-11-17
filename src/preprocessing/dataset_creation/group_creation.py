fe393bc39e754b160c414ce38e3533c51d6465f3
timlac <tim@lachmann.nu>
2022-11-17 10:59:13 +0100

    removed redudant time series handling file

--- src/preprocessing/dataset_creation/group_creation.py
+++ src/preprocessing/dataset_creation/group_creation.py
@@ -1,11 +1,9 @@
 import os
-import random
 
 import numpy as np
 import pandas as pd
 
-from global_config import seed, ROOT_DIR
-from sklearn.utils import shuffle
+from global_config import ROOT_DIR
 
 from src.preprocessing.dataset_creation.helpers import slice_by, get_fixed_col
 
@@ -17,17 +15,22 @@ def group_creation_desc(video_ids, groups, group_mapper):
     groups_unique, groups_counts = np.unique(groups, return_counts=True)
     counts_per_group = dict(zip(groups_unique, groups_counts))
 
-    print("Group assignment:")
-    print(group_mapper)
-
     print("Number of videos per video id:")
     print(counts_per_video_id)
+
+    print("Group assignment:")
+    print(group_mapper)
 
     print("Number of videos per group")
     print(counts_per_group)
 
 
 def create_groups(video_ids, n_groups):
+    """
+    :param video_ids: np array with video id for every video
+    :param n_groups: number of groups to create
+    :return: np array with group assignment for every video, same shape as video_ids
+    """
     group_mapper = {}
 
     for idx, video_id in enumerate(np.unique(video_ids)):
