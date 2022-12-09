3ef86f9271e74a5e96274340057c711b86d08ae9
timlac <tim@lachmann.nu>
2022-12-09 18:58:02 +0100

    completed new svm pipeline

--- src/analysis/supervised_learning/classification/svm_pipeline.py
+++ src/analysis/supervised_learning/classification/svm_pipeline.py
@@ -1,37 +1,28 @@
 import os
-import sys
-import pickle
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
 
-from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_validate
-from sklearn.utils import shuffle
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.model_selection import StratifiedKFold, KFold
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import StratifiedKFold
 from sklearn import metrics
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import confusion_matrix
 
 import seaborn as sns
 
-from global_config import ROOT_DIR, emotion_id_to_emotion_abr, conf_cmap, AU_INTENSITY_COLS, GAZE_COLS, POSE_COLS, \
+from global_config import ROOT_DIR, AU_INTENSITY_COLS, GAZE_COLS, POSE_COLS, \
     AUDIO_FUNCTIONALS_EGEMAPS_COLS, emotion_abr_to_emotion
 from src.analysis.supervised_learning.evaluation.confusion_matrix import ConfusionMatrixCreator, conf_mat_to_df, \
     plot_conf_matrix
 
-from src.analysis.data_exploration import plot_time_series_means_subplots
 
 from src.preprocessing.dataset_creation.scaling.functional_scaling import functional_scale_by
 from src.preprocessing.dataset_creation.scaling.low_level_scaling import low_level_scale_by
 
-from src.utils.helpers import mapper
 
 from src.preprocessing.dataset_creation.helpers import slice_by, get_cols, get_fixed_col
 from src.preprocessing.dataset_creation.aggregation import get_aggregate_measures
@@ -54,11 +45,11 @@ def param_search(x, y):
                   'kernel': kernel,
                   }
 
-    parameters = {'class_weight': ['balanced'],
-                  'C': [1],
-                  'gamma': [1],
-                  'kernel': ["linear"],
-                  }
+    # parameters = {'class_weight': ['balanced'],
+    #               'C': [1, 10],
+    #               'gamma': [1, 0.1],
+    #               'kernel': ["linear"],
+    #               }
 
     skf = StratifiedKFold(n_splits=5, shuffle=True)
 
@@ -74,23 +65,6 @@ def param_search(x, y):
     clf.fit(x, y)
     print(clf.best_params_)
     return clf
-
-
-def evaluate_scores(x, y, svc, splits, scoring_method):
-    scores = cross_validate(X=x, y=y,
-                            estimator=svc,
-                            scoring=[scoring_method],
-                            verbose=1,
-                            cv=splits,
-                            n_jobs=-1,
-                            return_train_score=True
-                            )
-
-    print('printing {} measures'.format(scoring_method))
-    print('avg (train):', np.mean(scores['train_{}'.format(scoring_method)]))
-    print('std (train):', np.std(scores['train_{}'.format(scoring_method)]))
-    print('avg (validation):', np.mean(scores['test_{}'.format(scoring_method)]))
-    print('std (validation):', np.std(scores['test_{}'.format(scoring_method)]))
 
 
 def check_alignment(sli, df):
@@ -109,17 +83,16 @@ df_video = pd.read_csv(path_video)
 path_audio = os.path.join(ROOT_DIR, "files/out/opensmile_functionals_query.csv")
 df_audio = pd.read_csv(path_audio)
 
-video_cols = [*AU_INTENSITY_COLS, *GAZE_COLS, *POSE_COLS]
-slices = slice_by(df_video, "filename")
-interpolator = Interpolator(video_cols)
-slices = interpolator.remove_interpolate(slices)
+slices_video = slice_by(df_video, "filename")
+interpolator = Interpolator([*AU_INTENSITY_COLS, *GAZE_COLS, *POSE_COLS])
+slices_video = interpolator.remove_interpolate(slices_video)
 
-slices, df_audio = align_audio_video(slices, df_audio)
+slices_video, df_audio = align_audio_video(slices_video, df_audio)
 
-check_alignment(slices, df_audio)
+check_alignment(slices_video, df_audio)
 
-y_audio_video = get_fixed_col(slices, "emotion_1_id")
-video_ids = get_fixed_col(slices, "video_id")
+y_audio_video = get_fixed_col(slices_video, "emotion_1_id")
+video_ids = get_fixed_col(slices_video, "video_id")
 
 
 def transform_video_modality(sli, cols):
@@ -133,23 +106,15 @@ def transform_video_modality(sli, cols):
     return scaler.fit_transform(x_agg)
 
 
-def run_modality(x, y):
-    clf = param_search(x, y)
-    svc = SVC(**clf.best_params_)
-    skf = StratifiedKFold(n_splits=5, shuffle=True)
-    splits = skf.split(x, y)
-    evaluate_scores(x, y, svc, splits, scoring_method="accuracy")
-    return clf, splits
-
-
 class Evaluator:
     conf_mat_save_path = os.path.join(ROOT_DIR,
-                                      "files/out/functionals/supervised_learning/confusion_matrix_all_modes")
+                                      "files/out/functionals/supervised_learning/confusion_matrix_")
 
-    def __init__(self, x, y, model_parameters):
+    def __init__(self, x, y, model_parameters, save_as):
         self.x = x
         self.y = y
         self.clf = SVC(**model_parameters, probability=True)
+        self.save_as = save_as
 
     def get_splits(self):
         skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=10)
@@ -172,19 +137,15 @@ class Evaluator:
         conf_mat_creator = ConfusionMatrixCreator(self.clf)
         cm = conf_mat_creator.calculate_avg_conf_matrix(self.x, self.y, splits)
         df_cm = conf_mat_to_df(cm, self.y)
-        df_cm.to_csv(self.conf_mat_save_path + ".csv", index=False)
-        plot_conf_matrix(df_cm, title="Normalized Confusion Matrix", save_path=self.conf_mat_save_path)
+        df_cm.to_csv(self.conf_mat_save_path + self.save_as + ".csv", index=False)
+        plot_conf_matrix(df_cm, title="Normalized Confusion Matrix", save_path=self.conf_mat_save_path + self.save_as)
 
     def create_classification_report(self):
         splits = self.get_splits()
         y_pred = cross_val_predict(self.clf, self.x, self.y, cv=splits)
         report = metrics.classification_report(y_true=self.y, y_pred=y_pred,
                                                target_names=emotion_abr_to_emotion.values())
-        print(report)
 
-        matrix = confusion_matrix(self.y, y_pred)
-        df = pd.DataFrame(matrix)
-        df.to_csv(self.conf_mat_save_path + "_custom.csv")
 
 
 def print_scores(scores, scoring_method):
@@ -195,28 +156,26 @@ def print_scores(scores, scoring_method):
     print('std (validation):', np.std(scores['test_{}'.format(scoring_method)]))
 
 
-# au = transform_video_modality(slices, AU_INTENSITY_COLS)
-# run_modality(au, y_audio_video)
-#
-# gaze = transform_video_modality(slices, GAZE_COLS)
-# run_modality(gaze, y_audio_video)
-#
-# pose = transform_video_modality(slices, POSE_COLS)
-# run_modality(pose, y_audio_video)
-#
-au_gaze_pose = transform_video_modality(slices, [*AU_INTENSITY_COLS, *GAZE_COLS, *POSE_COLS])
-# run_modality(au_gaze_pose, y_audio_video)
-#
+for col in [("action_units", AU_INTENSITY_COLS), ("gaze", GAZE_COLS), ("pose", POSE_COLS)]:
+    print(col[0])
+    x = transform_video_modality(slices_video, col[1])
+    clf = param_search(x, y_audio_video)
+    eval = Evaluator(x, y_audio_video, clf.best_params_, col[0])
+    eval.evaluate_scores("accuracy")
+    eval.evaluate_scores("roc_auc_ovo_weighted")
+
+
+au_gaze_pose = transform_video_modality(slices_video, [*AU_INTENSITY_COLS, *GAZE_COLS, *POSE_COLS])
+
 egemaps = df_audio[AUDIO_FUNCTIONALS_EGEMAPS_COLS].values
 egemaps = functional_scale_by(egemaps, video_ids, "min_max")
-# run_modality(egemaps, y_audio_video)
 
 all_modes = np.hstack((au_gaze_pose, egemaps))
 
 
 clf = param_search(all_modes, y_audio_video)
 
-eval = Evaluator(all_modes, y_audio_video, clf.best_params_)
+eval = Evaluator(all_modes, y_audio_video, clf.best_params_, "some_mode")
 eval.evaluate_scores("accuracy")
 eval.evaluate_scores("roc_auc_ovo_weighted")
 eval.evaluate_confusion_matrix()
